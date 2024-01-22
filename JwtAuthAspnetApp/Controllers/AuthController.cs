b992072b69045aa206f3c289fc9e55e708944a90
AlokAnil33 <alok.anil.33@gmail.com>
2024-01-22 17:54:14 +0530

    Final Commit V1

---
+++ JwtAuthAspnetApp/Controllers/AuthController.cs
@@ -0,0 +1,144 @@
+﻿using JwtAuthAspnetApp.Core.DataModel;
+using JwtAuthAspnetApp.Core.OtherObjects;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace JwtAuthAspnetApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+
+
+        //seeding user roles 
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            bool isOwnerRoleExists = await roleManager.RoleExistsAsync(StaticUserRoles.OWNER);
+            bool isAdminRoleExists = await roleManager.RoleExistsAsync(StaticUserRoles.ADMIN);
+            bool isUserRoleExists = await roleManager.RoleExistsAsync(StaticUserRoles.USER);
+
+            if (isOwnerRoleExists && isAdminRoleExists && isUserRoleExists)
+                return Ok("roles seeding is already done");
+
+            await roleManager.CreateAsync(new IdentityRole(StaticUserRoles.USER));
+            await roleManager.CreateAsync(new IdentityRole(StaticUserRoles.ADMIN));
+            await roleManager.CreateAsync(new IdentityRole(StaticUserRoles.OWNER));
+
+            return Ok("role seeding successfull");
+        }
+
+        //Route -> Register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            var isExistUser = await userManager.FindByNameAsync(registerModel.UserName);
+            if (isExistUser != null)
+                return BadRequest("UserName already exists");
+            IdentityUser newUser = new IdentityUser()
+            {
+                Email = registerModel.EMail,
+                UserName = registerModel.UserName,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+
+            var createUserResult = await userManager.CreateAsync(newUser, registerModel.Password);
+            if (!createUserResult.Succeeded)
+            {
+                var errorString = "User Creation failed because :";
+                foreach (var error in createUserResult.Errors)
+                {
+                    errorString += "\n#" + error.Description;
+                }
+                return BadRequest(errorString);
+            }
+            //Add default USER role to all users
+            await userManager.AddToRoleAsync(newUser, StaticUserRoles.USER);
+            return Ok("User craeted succesfully");
+        }
+
+        //Route -> Login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await userManager.FindByNameAsync(loginModel.UserName);
+            if (user is null)
+                return Unauthorized("Invalid Credentials");
+            var isPasswordCorrect = await userManager.CheckPasswordAsync(user, loginModel.Password);
+            if (!isPasswordCorrect)
+                return Unauthorized("Invalid Credentials");
+            var userRoles = await userManager.GetRolesAsync(user);
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim("JWTID", Guid.NewGuid().ToString())
+            };
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+            var token = GenerateNewJsonWebToken(authClaims);
+            return Ok(token);
+        }
+
+        private string GenerateNewJsonWebToken(List<Claim> claims)
+        {
+            var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+            var tokenObject = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: claims,
+                signingCredentials: new SigningCredentials(authSecret, SecurityAlgorithms.HmacSha256)
+                );
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+            return token;
+        }
+
+        //route -> make user -> admin
+        [HttpPost]
+        [Route("make-admin")]
+        public async Task<IActionResult> MakeAdmin([FromBody] UpdatePermissionModel updatePermissionModel)
+        {
+            var user = await userManager.FindByNameAsync(updatePermissionModel.UserName);
+            if (user is null)
+                return BadRequest("Invalid Username");
+            await userManager.AddToRoleAsync(user, StaticUserRoles.ADMIN);
+            return Ok("User is now an Admin");
+        }
+
+        //route -> make user -> owner
+        [HttpPost]
+        [Route("make-owner")]
+        public async Task<IActionResult> MakeOwner([FromBody] UpdatePermissionModel updatePermissionModel)
+        {
+            var user = await userManager.FindByNameAsync(updatePermissionModel.UserName);
+            if (user is null)
+                return BadRequest("Invalid Username");
+            await userManager.AddToRoleAsync(user, StaticUserRoles.OWNER);
+            return Ok("User is now an Owner");
+        }
+
+
+    }
+}
--- JwtAuthAspnetApp/Program.cs
+++ JwtAuthAspnetApp/Program.cs
@@ -1,3 +1,11 @@
+//program 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using JwtAuthAspnetApp.Core.DBContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,51 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add DB
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("local");
+    options.UseSqlServer(connectionString);
+});
+
+//AddIdentity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDBContext>()
+                .AddDefaultTokenProviders();
+
+//Configure Identity
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.SignIn.RequireConfirmedEmail = false;
+});
+
+//Add Authentication & JWTBearer
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
 
 var app = builder.Build();
 
@@ -17,6 +70,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
